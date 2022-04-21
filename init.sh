6f13e7d63c033216c433a4e8583c8b2896a147a4
hernaneg350 <hernaneg350@gmail.com>
2022-04-21 00:33:34 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 xcode-select -p > /dev/null 2>&1
 
-if [[ $0 > 0 ]] ; then
+if [[ $? > 0 ]] ; then
     echo "X-Code Command Line Tools not installed. You will be redirected to installation. Please call this script again once finished."
     xcode-select --install > /dev/null 2>&1
     exit 0
