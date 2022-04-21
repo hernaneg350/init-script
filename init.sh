7e991d8bc15c4b3f18834a6e860cc75cc769b8e3
hernaneg350 <hernaneg350@gmail.com>
2022-04-20 23:38:08 -0300

    Test with x-code install

--- init.sh
+++ init.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-xcode-select --install
+xcode-select -p 1>/dev/null
+
+if [[ $0 > 0 ]] ; then
+    echo "X-Code Command Line Tools not installed. You will be redirected to installation. Please call this script again once finished."
+    xcode-select --install
+    exit 0
+fi
 
 echo "Provide your PAT..."
 read PAT
 git -C $HOME clone --recurse-submodules --separate-git-dir=$HOME/.tracker https://$PAT@github.com/hernaneg350/home.git
+
+
