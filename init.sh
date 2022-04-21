92ad0bd00d6037b4dc0d5d9ac85c888932209fac
hernaneg350 <hernaneg350@gmail.com>
2022-04-20 23:47:38 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Executed in sub-shell since passing in the script to bash using pipes (i.e. curl https://rawgithub... | bash) will mess up input redirection.
-(xcode-select -p > /dev/null 2>&1)
+#(xcode-select -p > /dev/null 2>&1)
 
-if [[ $0 > 0 ]] ; then
-    echo "X-Code Command Line Tools not installed. You will be redirected to installation. Please call this script again once finished."
-    (xcode-select --install > /dev/null 2>&1)
-    exit 0
-fi
+#if [[ $0 > 0 ]] ; then
+#    echo "X-Code Command Line Tools not installed. You will be redirected to installation. Please call this script again once finished."
+#    (xcode-select --install > /dev/null 2>&1)
+#    exit 0
+#fi
 
 echo "Provide your PAT..."
 read PAT
