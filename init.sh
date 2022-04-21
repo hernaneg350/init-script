958536e4cdb107fea4cfabac8131227e01a8805f
hernaneg350 <hernaneg350@gmail.com>
2022-04-21 00:35:54 -0300

    Move initialisation to a two-step process
    This has proven to be better, first we make sure xcode is installed, or else we
    start the installation. Then we encourage the user to continue after
    installation is done.

--- init.sh
+++ init.sh
@@ -5,14 +5,14 @@ xcode-select -p > /dev/null 2>&1
 if [[ $? > 0 ]] ; then
     echo "X-Code Command Line Tools not installed. You will be redirected to installation. Please call this script again once finished."
     xcode-select --install > /dev/null 2>&1
+else
+    echo "Provide your PAT..."
+    read PAT
+    rm -rf .tracker # Clean for idempotency
+    GIT_DIR=.tracker git -C $HOME init
+    alias home="git -C $HOME --git-dir=.tracker --work-tree=."
+    home remote add origin https://$PAT@github.com/hernaneg350/home.git
+    home fetch origin
+    home checkout -f origin/master
+    home remote set-url origin git@github.com:hernaneg350/home.git
 fi
-
-echo "Provide your PAT..."
-read PAT
-rm -rf .tracker # Clean for idempotency
-GIT_DIR=.tracker git -C $HOME init
-alias home="git -C $HOME --git-dir=.tracker --work-tree=."
-home remote add origin https://$PAT@github.com/hernaneg350/home.git
-home fetch origin
-home checkout -f origin/master
-home remote set-url origin git@github.com:hernaneg350/home.git
